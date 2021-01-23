f885df5a4e86cc190dbb0e793867cfd7c6ae2648
Juan Diego Coto <jd506@hotmail.com>
2021-01-23 23:54:21 +0000

    Uploading project

--- src/main.js
+++ src/main.js
@@ -5,6 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let subject = ["the dog", "my granma", "his turtle", "my bird"];
+  let action = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    subject[subjectIndex] + " " + action[actionIndex] + " " + when[whenIndex]
+  );
 };
